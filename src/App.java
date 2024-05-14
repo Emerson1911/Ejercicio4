c7447317e548695d8ddc66310f89a3d966755df0
Emerson1911 <1284662@clases.edu.sv>
2024-05-13 20:36:12 -0600

    Codigo documentado

--- src/App.java
+++ src/App.java
@@ -1,29 +1,31 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner(System.in);
+public class App { // Define una clase llamada App
+    public static void main(String[] args) throws Exception { // Define el método principal main
+        Scanner teclado = new Scanner(System.in); // Crea un objeto Scanner llamado 'teclado' para leer la entrada del usuario
 
-        //System.out.println("Por favor ingese el tamaño del array:");
-        //int n = teclado.nextInt();
-         int[] array= new int[10]; 
+        int[] array = new int[10]; // Crea un array de enteros de tamaño 10
 
-        for (int i = 0; i < array.length; i++){
-            System.out.print("Ingrese dato"+(i+1)+": ");
-            array [i] = teclado.nextInt();
+        // Solicita al usuario que ingrese diez números y los almacena en el array
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Ingrese dato" + (i + 1) + ": "); // Solicita al usuario que ingrese un número
+            array[i] = teclado.nextInt(); // Lee el número ingresado por el usuario y lo almacena en el array
         }
 
-        int mayor,menor;
-        mayor = menor = array [0];
+        int mayor, menor; // Declara variables para almacenar el mayor y el menor número
+        mayor = menor = array[0]; // Inicializa las variables 'mayor' y 'menor' con el primer elemento del array
 
-        for (int i = 0; i < array.length; i++){
-            if(array [i] > mayor){
-                mayor = array[i];
-            }if(array [i] < menor){
-                menor = array[i]; 
+        // Itera sobre el array para encontrar el mayor y el menor número
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > mayor) { // Comprueba si el elemento actual es mayor que el mayor encontrado hasta ahora
+                mayor = array[i]; // Si es así, actualiza el valor de 'mayor'
+            }
+            if (array[i] < menor) { // Comprueba si el elemento actual es menor que el menor encontrado hasta ahora
+                menor = array[i]; // Si es así, actualiza el valor de 'menor'
             }
         }
-        System.out.println("El mayor valor es :"+mayor);
-        System.out.println("El menor valor es :"+menor);
+        // Imprime el mayor y el menor número encontrados
+        System.out.println("El mayor valor es :" + mayor);
+        System.out.println("El menor valor es :" + menor);
     }
 }
